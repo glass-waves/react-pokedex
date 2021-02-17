9d109045a1b266322d794e7be913c953070a0ccc
Dylan White <dylan.whitej@gmail.com>
2021-02-16 17:26:21 -0800

    fixes page problem

--- src/Search.js
+++ src/Search.js
@@ -13,6 +13,10 @@ export default class Search extends Component {
         currentFilter: '',
         search: '',
         loading: false,
+        page: 1,
+        perPage: 0,
+        count: 0,
+        totalPages: 0,
     }
     componentDidMount = async () => {
         await this.fetchPokemon();
@@ -24,14 +28,20 @@ export default class Search extends Component {
 
         this.setState({ loading: true })
 
-        const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.search}&sort=${this.state.currentFilter}&direction=${this.state.filterDirection}`);
+        const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.search}&sort=${this.state.currentFilter}&direction=${this.state.filterDirection}&page=${this.state.page}`);
 
         this.setState({
             loading: false,
             data: data.body.results,
+            count: data.body.count,
+            perPage: data.body.perPage,
+            totalPages: data.body.count > 20 ? Math.floor(data.body.count / data.body.perPage) : 1
         })
     }
     handleButtonPress = async () => {
+        this.setState({
+            page: 1,
+        })
         await this.fetchPokemon();
     }
     handleSearchChange = (e) => {
@@ -51,52 +61,50 @@ export default class Search extends Component {
             currentFilter: e.target.value
         })
     }
-
-    // sortBy = () => {
-    //     if (this.state.currentFilter === '' || this.state.filterDirection === '') return;
-
-    //     if (this.state.currentFilter === "attack" || this.state.currentFilter === "defense" || this.state.currentFilter === "hp") {
-    //         if (this.state.filterDirection === 'ascending') {
-    //             this.state.data.sort((a, b) => a[this.state.currentFilter] - b[this.state.currentFilter])
-    //         } else {
-    //             this.state.data.sort((a, b) => b[this.state.currentFilter] - a[this.state.currentFilter])
-    //         }
-    //     } else {
-    //         if (this.state.filterDirection === 'ascending') {
-    //             this.state.data.sort((a, b) => a[this.state.currentFilter].localeCompare(b[this.state.currentFilter]))
-    //         } else {
-    //             this.state.data.sort((a, b) => b[this.state.currentFilter].localeCompare(a[this.state.currentFilter]))
-    //         }
-    //     }
-    // }
-    // filterArray = () => {
-    //     if (this.state.search === '') return this.state.data;
-    //     const filteredArr = this.state.data.filter(pokemon => {
-    //         console.log(pokemon.pokemon)
-    //         if (pokemon.pokemon.includes(this.state.search)) return true;
-    //     })
-    //     return filteredArr;
-    // }
-
+    handlePrev = async () => {
+        if (this.state.page === 1) {
+            return;
+        } else {
+            await this.setState({
+                page: this.state.page - 1
+            })
+        }
+        await this.fetchPokemon();
+    }
+    handleNext = async () => {
+        if (this.state.page === this.state.totalPages) {
+            return;
+        } else {
+            await this.setState({
+                page: (this.state.page + 1)
+            })
+        }
+        await this.fetchPokemon();
+    }
     render() {
-
-        // this.sortBy();
-        // const filteredList = this.filterArray();
-
-
         return (
             <div className="main">
                 <section className="sidebar">
                     <h2>PokeSearch</h2>
+
                     <label for={this.state.search}> Search by keyword </label>
                     <input value={this.state.search} onChange={this.handleSearchChange} />
+
                     <Dropdown filterList={['asc', 'desc']} handleChange={this.handleFilterDirection} keyName="Sort Direction" />
                     <Dropdown filterList={this.state.listOfFilters} handleChange={this.handleFilterChange} keyName="Sort By" />
+
                     <button onClick={this.handleButtonPress}>Search</button>
                 </section>
 
                 <section className="pokeDisplay">
-                    {this.state.loading?<Spinner /> : <PokeList filteredList={this.state.data} />}
+
+                    <p className="pages">Page: {this.state.page} of {this.state.totalPages}</p>
+                    <button onClick={this.handlePrev}>Previous Page</button>
+                    <button onClick={this.handleNext}>Next Page</button>
+
+
+                    {this.state.loading ? <Spinner /> : <PokeList filteredList={this.state.data} />}
+
                 </section>
             </div >
         )
